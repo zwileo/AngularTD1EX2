143309a303e482a12a6729b15b3d543f2ebc991c
=zwileo <=leo.zwilling@orange.fr>
2023-03-27 08:48:38 +0200

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TD1ex2';
+
+  nomImage:String="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg=="
+
+  
+
+  
+  onClick():void
+  {
+      console.log("Bouton cliqué");
+  }
+
+  etudiant:any[] = [
+    {"nom":"Dupond", "prenom":"Jean", "email":"Dupond.d@gmail.com", "age":20,"telephone":"0645255532"},
+    {"nom":"Dupont", "prenom":"Fred", "email":"Dupond.t@gmail.com", "age":24,"telephone":"0654877622"},
+    {"nom":"Unknown", "prenom":"Constancia", "email":"constancia@unknown.com", "age":31,"telephone":"0678365849"},
+    {"nom":"Astier", "prenom":"Alexandre", "email":"alex.a@gmail.com", "age":25,"telephone":"0614628597"}
+  ]
+
+
+  bouton:boolean=true;
+
 }
